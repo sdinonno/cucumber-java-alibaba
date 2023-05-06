069cb4e4e2b84287d9a7dc5c9661eb5c74f7c511
Sofia Di Nonno <sofia@thelucky.app>
2023-05-06 15:37:00 -0300

    update test runner class

--- src/test/java/runner/TestsRunner.java
+++ src/test/java/runner/TestsRunner.java
@@ -2,7 +2,13 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features="src/test/resources/features/Search.feature",glue="stepsdefs")
 public class TestsRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
